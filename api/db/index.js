69c9b0ad7c82c9f2bf064c4ae2f638adee9cd5c7
Matt Startin <matt.startin@oneserve.co.uk>
2020-11-23 13:22:04 +0000

    Remove mongo connection

--- api/db/index.js
+++ api/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://nextgen:G0ldThund3r!@ds123796.mlab.com:23796/osnextgendb', { useNewUrlParser: true })
+    .connect('SOME_MONGO_CONNECTION', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
